fddb2751ee6010a16cc7864ea28b2583340304df
Magne Sjaastad <magne.sjaastad@ceetronsolutions.com>
2023-06-30 11:39:58 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 // main.cpp
-#include <sqlite3.h>
+//#include <sqlite3.h>
 #include <cstdio>
-#include <fmt/core.h>
+//#include <fmt/core.h>
 
 // main function!
 int main()
 {
-    fmt::print("Hello, world!\n");
-    printf("%s\n", sqlite3_libversion());
+    //fmt::print("Hello, world!\n");
+    printf("test");
     return 0;
 }
